315354c8225d8a0797fdccdd63837a1d38e9ac5e
Guille <guille.fer79@hotmail.com>
2023-08-14 21:45:21 -0300

    agregamos JavaScript al proyecto, y usamos animaciones

---
+++ JavaScript/animaciones.js
@@ -0,0 +1,23 @@
+window.sr =ScrollReveal();
+
+    sr.reveal('.title-inicio', {
+        duration: 1700,
+        origin:'right',
+        distance: '-100px',
+    });
+    sr.reveal('.contenedor-header', {
+        duration: 1700,
+        origin:'bottom',
+        distance: '-100px',
+    });
+    sr.reveal('.contenido-nosotros', {
+        duration: 1700,
+        origin:'right',
+        distance: '-100px',
+    });
+    sr.reveal('.contenedor_card', {
+        duration: 1700,
+        origin:'right',
+        distance: '-100px',
+    });
+
