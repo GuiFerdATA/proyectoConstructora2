af8a59a69d599344319e0a3d77dcc32ca9329c98
Guille <guille.fer79@hotmail.com>
2023-08-23 19:42:07 -0300

    .

--- JavaScript/animaciones.js
+++ JavaScript/animaciones.js
@@ -1,28 +1,36 @@
-window.sr =ScrollReveal();
+window.sr = ScrollReveal();
 
-    sr.reveal('.title-inicio', {
-        duration: 2000,
-        origin:'right',
-        distance: '-100px',
-    });
-    sr.reveal('.contenedor-header', {
-        duration: 1000,
-        origin:'bottom',
-        distance: '-100px',
-    });
-    sr.reveal('.contenido-nosotros', {
-        duration: 1700,
-        origin:'top',
-        distance: '-50px',
-    });
-    sr.reveal('.animationtitle', {
-        duration: 2000,
-        origin:'right',
-        distance: '-20px',
-    });
-    sr.reveal('.titleAnimado', {
-        duration: 2000,
-        origin:'right',
-        distance: '-20px',
-    });
+
+sr.reveal('.contenedor-header', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '-100px',
+});
+sr.reveal('.title-inicio', {
+    duration: 2000,
+    origin: 'right',
+    distance: '-100px',
+});
+
+sr.reveal('.contenido-nosotros', {
+    duration: 1700,
+    origin: 'top',
+    distance: '-50px',
+});
+sr.reveal('.animationtitle', {
+    duration: 2000,
+    origin: 'right',
+    distance: '-20px',
+});
+sr.reveal('.titleAnimado', {
+    duration: 2000,
+    origin: 'right',
+    distance: '-20px',
+});
+sr.reveal('.titleAnimadoC', {
+    duration: 1000,
+    origin: 'right',
+    distance: '-20px',
+});
+
 
